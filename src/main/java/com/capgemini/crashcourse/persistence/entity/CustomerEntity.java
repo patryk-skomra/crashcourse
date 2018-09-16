5158379e6162851e0289ce53351051ec5859c4f0
Skomra <patryk.skomra@capgemini.com>
2018-09-16 10:42:35 +0200

    abstract entity

--- src/main/java/com/capgemini/crashcourse/persistence/entity/CustomerEntity.java
+++ src/main/java/com/capgemini/crashcourse/persistence/entity/CustomerEntity.java
@@ -2,25 +2,44 @@ package com.capgemini.crashcourse.persistence.entity;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.Inheritance;
-import javax.persistence.InheritanceType;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "CUSTOMER")
-@Inheritance(strategy = InheritanceType.JOINED)
 public class CustomerEntity extends AbstractEntity {
 
-	@OneToOne
-	@JoinColumn(name = "ADDRESS_ID")
-	private AddressEntity address;
+	@Column(nullable = false)
+	private String firstName;
+
+	@Column(nullable = false)
+	private String lastName;
 
 	private String email;
 
 	@Column(nullable = false)
 	private String telephoneNrumber;
+
+	@OneToOne
+	@JoinColumn(name = "ADDRESS_ID")
+	private AddressEntity address;
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
 
 	public AddressEntity getAddress() {
 		return address;
